f7698ede8f6e6b7c60e1dd170012bf9060c417b0
eddigio <39969878+eddigio@users.noreply.github.com>
2020-11-16 21:56:32 -0500

    Agregar archivos de proyecto.

---
+++ EdisonMendoza_examen/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EdisonMendoza_examen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_ventana_dos_Clicked(object sender, EventArgs e)
+        {
+            string user = txtuser.Text;
+            string pass = txtpass.Text;
+
+            await Navigation.PushAsync (new Ventana_dos(user,pass));
+        }
+    }
+}
---
+++ EdisonMendoza_examen/Ventana_dos.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EdisonMendoza_examen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ventana_dos : ContentPage
+    {
+        public Ventana_dos(string user,string pass)
+        {
+            InitializeComponent();
+            lbUser.Text = user;
+            txtpass.Text = pass;
+
+        }
+    }
+}
